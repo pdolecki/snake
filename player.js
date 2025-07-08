88e77feceadc2b8db799594f5add741dc5b5068c
Dolecki Patryk <pdolecki.it@gmail.com>
2025-07-08 10:01:00 +0200

    Created boundaries of the battle field

--- player.js
+++ player.js
@@ -8,11 +8,22 @@ class Player {
     this.color = color;
     this.width = this.game.cellSize;
     this.height = this.game.cellSize;
+    this.moving = true;
   }
 
   update() {
-    this.x += this.speedX;
-    this.y += this.speedY;
+    const leftBoundary = this.x <= 0 && this.speedX < 0;
+    const rightBoundary = this.x >= this.game.columns - 1 && this.speedX > 0;
+    const topBoundary = this.y <= 0 && this.speedY < 0;
+    const bottomBoundary = this.y >= this.game.rows - 1 && this.speedY > 0;
+    if (leftBoundary || rightBoundary || topBoundary || bottomBoundary) {
+      this.moving = false;
+    }
+
+    if (this.moving) {
+      this.x += this.speedX;
+      this.y += this.speedY;
+    }
   }
 
   draw() {
@@ -28,21 +39,25 @@ class Player {
   turnUp() {
     this.speedX = 0;
     this.speedY = -1;
+    this.moving = true;
   }
 
   turnDown() {
     this.speedX = 0;
     this.speedY = 1;
+    this.moving = true;
   }
 
   turnLeft() {
     this.speedX = -1;
     this.speedY = 0;
+    this.moving = true;
   }
 
   turnRight() {
     this.speedX = 1;
     this.speedY = 0;
+    this.moving = true;
   }
 }
 
